9c911671bd07590abdb7d19bf58df37fbccd52d7
Felipe Santos <felipesales007@hotmail.com>
2025-08-20 21:18:34 +0200

    crudo basico

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { Prisma } from '@prisma/client';
 
@@ -7,7 +15,27 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() user: Prisma.UserCreateInput) {
+  async create(@Body() user: Prisma.UserCreateInput) {
     return this.userService.create(user);
+  }
+
+  @Get()
+  async findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    return this.userService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() user: Prisma.UserCreateInput) {
+    return this.userService.update(id, user);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    return this.userService.delete(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,7 +5,31 @@ import { PrismaService } from 'src/prisma.servce';
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
+
   async create(user: Prisma.UserCreateInput) {
     return await this.prisma.user.create({ data: user });
+  }
+
+  async findAll() {
+    return await this.prisma.user.findMany();
+  }
+
+  async findOne(id: number) {
+    return await this.prisma.user.findUnique({ where: { id: Number(id) } });
+  }
+
+  async update(id: number, user: Prisma.UserCreateInput) {
+    return await this.prisma.user.update({
+      where: { id: Number(id) },
+      data: {
+        name: user.name,
+        email: user.email,
+        description: user.description,
+      },
+    });
+  }
+
+  async delete(id: number) {
+    return await this.prisma.user.delete({ where: { id: Number(id) } });
   }
 }
