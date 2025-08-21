b02d6fc3a8f41fa9dd2b89da8a94ecd04e8f7cb8
Felipe Santos <felipesales007@hotmail.com>
2025-08-21 15:16:31 +0200

    views com Handlebars

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   //uso global de interceptor, e a serialização dos dados nas requisições.
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  app.useStaticAssets(join(__dirname, '..', 'public')); //arquivos públicos
+  app.setBaseViewsDir(join(__dirname, '..', 'views')); //views
+  app.setViewEngine('hbs');
+
   await app.listen(3000);
 }
 bootstrap();
